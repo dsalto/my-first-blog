95c48b641e875fdc70820eca6abb6650481dc6ec
dsalto <dsaltocardenas@unomaha.edu>
2020-01-31 13:48:00 -0600

    Changed the HTML for the site in blog/tmeplates/blog/post_list.html

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+"""Here we're importing Django's function path and all 
+of our views from the blog application.
+"""
+from django.urls import path
+from . import views
+
+
+"""URL pattern"""
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
